17f07496efcaca53c765ce8b5cc2e45a5de4dbd7
Patryk Nizio <dyzio18@gmail.com>
2017-04-30 15:53:14 +0300

    v0.1.2
    * Timer
    * Check answer
    * Layout upgrade

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 /* JavaScript Quiz
- * version 0.1.0
+ * version 0.1.2
  * http://patryk-nizio.pl/
  * created by: Patryk Nizio @dyzio
  */
-
-/* TODO:
-    [ ] Timer, dodac zegar, + animacja gdy koncowka  
-
-    optionnal [] RestartGame()
-*/
-
 
 (function(){
 
@@ -36,57 +29,106 @@
     // Current user score
     var userScore = 0;
  
-    var msg = "Witaj! Weź udział w quizie i odpowiedz na pytania. Pokaż że jesteś geniuszem, ale uważaj czas szybko leci. Powodzenia!"
+    // Welcome screen 
+    var msg = "Weź udział w quizie i odpowiedz na pytania. Pokaż że jesteś geniuszem, ale uważaj czas szybko leci.<br> Powodzenia!"
     showQuestion (queryDiv, msg);
     document.getElementById("id__timer").innerHTML = timeSec;
-
     updateProgress(progressDiv,countQuery+1,numQuery);
 
     var clicked = false;
+    var checked = false;
     const btnNext = document.getElementById("id__buttonNext");
+   
     btnNext.addEventListener('click',(e)=> {
         if(!clicked) {
             clicked = true;
             btnNext.innerHTML = "Dalej";
-            timer(timeSec,true);
+            
+            if(!checked){
+                timer(timeSec);
+            }
+            else{
+                document.getElementById("id__questionBox").style.display = 'block';               
+            }
+        }  
 
-        }      
         handleView();
     })
 
     answerParentDiv.addEventListener('click', function(e){
         if(e.target.className === "quiz__answer"){
             userScore += checkAnswer(e.target.getAttribute("data-answer")-1, quizData.questions[countQuery].answers );
-            console.log(userScore);
             handleView();
         }
     }) 
 
-    function handleView(){
-        if( countQuery < numQuery -1 ){
-            countQuery++;  
-            showQuestion(queryDiv, quizData.questions[countQuery].question);
-            showAnswer(answerParentDiv, quizData.questions[countQuery].answers); 
-            updateProgress(progressDiv,countQuery+1,numQuery);
-        }
-        else {
-            var timer = document.getElementById("id__timer").remove(); //!!!
-
-            btnNext.remove();
-            var questionBox = document.getElementById("id__questionBox");
-            showStats (answerParentDiv,questionBox, userScore, numQuery);
-        }
+/* Show current view
+*/
+function handleView(){
+    // If questions to ask
+    if( countQuery < numQuery -1 ){        
+        countQuery++;  
+        showQuestion(queryDiv, quizData.questions[countQuery].question);
+        showAnswer(answerParentDiv, quizData.questions[countQuery].answers, checked); 
+        updateProgress(progressDiv,countQuery+1,numQuery);
     }
+    // End of questions
+    else {
+        if(!checked){
+            var timer = document.getElementById("id__timer").remove();
+        }
+        clicked = false;
+        checked = true;
+        countQuery = -1; // Restart counter
+
+        btnNext.innerHTML = "Sprawdź odpowiedzi";
+        var questionBox = document.getElementById("id__questionBox");
+        showStats (answerParentDiv,questionBox, userScore, numQuery);
+    }
+}
+
+/*  Timer Function (countdown/critical time/stop game)
+*   @param: time - number (eg. read from JSON file)
+*                  !default - number of questions multipled by 30 sec.
+*/
+function timer(time = numQuery*30 ){ 
+    var timer = document.getElementById("id__timer");
+    var tm = time;
+    var interval = setInterval(function() {
+        tm--;
+        timer.innerHTML = tm;
+        if ( tm <= 30 ){
+            timer.className = "sg-text-bit  sg-text-bit--not-responsive sg-text-bit--small  quiz__text--warning";
+        }
+        if (tm === 0 ){
+            clearInterval(interval);
+            countQuery = numQuery;
+            handleView();
+            return;    
+        }
+    }, 1000);
+}
 })();
 
 /*
 *   @param: element - DOM string
 *   @param: list - string array
+*   @param: check - boleans
 */
-function showAnswer (element, list) {
+function showAnswer (element, list, check = false) {
     var msg = "";
     for(var i = 0; i < list.length; i++){
-        msg += '<p class="quiz__answer" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
+        if(check) {
+            if(list[i].correct){
+                msg += '<p class="quiz__answer quiz__answer--correct" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
+            }
+            else {
+                msg += '<p class="quiz__answer" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
+            }
+        }
+        else{
+            msg += '<p class="quiz__answer" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
+        }
     }
     element.innerHTML = msg;
 }
@@ -110,8 +152,14 @@ function showQuestion (element,  str) {
     element.innerHTML = str;    
 }
 
+/* Show statistic in final screen
+*   @param: element1 - DOM string (output)
+*   @param: element2 - DOM string
+*   @param: score - number
+*   @param: maxNum - number
+*/
 function showStats (element1, element2, score, maxNum) {
-    element1.innerHTML = '<h1 class="sg-text-bit sg-text-bit--not-responsive sg-text-bit--warning"> Twój wynik: '+score+'</h1>';
+    element1.innerHTML = '<h1 class="sg-text-bit sg-text-bit--not-responsive sg-text-bit--warning quiz__text--center"> Twój wynik: '+score+"/"+maxNum+'</h1>';
     element2.style.display = 'none';
 }
 
@@ -149,21 +197,3 @@ function updateProgress(element, counter, maxNum){
     var msg = counter+"/"+maxNum;
     element.innerHTML = msg;
 }
-
-/*  
-*   @param: time - number (read from JSON file)
-*/
-function timer(time = 0, play = false){
-    var timer = document.getElementById("id__timer");
-    var tm = time;
-    console.log(time);
-    var interval = setInterval(function() {
-        tm--;
-        timer.innerHTML = tm;
-        if (tm === 0 ){
-            clearInterval(interval);
-            return;    
-        } 
-    }, 1000);    //TODO 
-}
-
