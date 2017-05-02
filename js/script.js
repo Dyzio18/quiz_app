2e17fc9734e55be69d30e16ddeac72062752cfe0
Patryk Nizio <dyzio18@gmail.com>
2017-05-02 12:50:24 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 (function(){
 
     const urlJSON = "https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json" 
-    var qData = readFile(urlJSON);
+    let qData = readFile(urlJSON);
 
     if(!qData)
     {
@@ -25,18 +25,18 @@
     // Time in secnds 
     const timeSec = quizData.time_seconds;
     // Current question counter
-    var countQuery = -1;
+    let countQuery = -1;
     // Current user score
-    var userScore = 0;
+    let userScore = 0;
  
     // Welcome screen 
-    var msg = "Weź udział w quizie i odpowiedz na pytania. Pokaż że jesteś geniuszem, ale uważaj czas szybko leci.<br> Powodzenia!"
+    let msg = 'Weź udział w quizie i odpowiedz na pytania. Pokaż że jesteś geniuszem, ale uważaj czas szybko leci.<br> Powodzenia!';
     showQuestion (queryDiv, msg);
     document.getElementById("id__timer").innerHTML = timeSec;
     updateProgress(progressDiv,countQuery+1,numQuery);
 
-    var clicked = false;
-    var checked = false;
+    let clicked = false;
+    let checked = false;
     const btnNext = document.getElementById("id__buttonNext");
    
     btnNext.addEventListener('click',(e)=> {
@@ -46,12 +46,10 @@
             
             if(!checked){
                 timer(timeSec);
-            }
-            else{
+            } else{
                 document.getElementById("id__questionBox").style.display = 'block';               
             }
         }  
-
         handleView();
     })
 
@@ -64,7 +62,7 @@
 
 /* Show current view
 */
-function handleView(){
+const handleView  = function handleView(){
     // If questions to ask
     if( countQuery < numQuery -1 ){        
         countQuery++;  
@@ -75,29 +73,30 @@ function handleView(){
     // End of questions
     else {
         if(!checked){
-            var timer = document.getElementById("id__timer").remove();
+            let timer = document.getElementById("id__timer").remove();
         }
         clicked = false;
         checked = true;
         countQuery = -1; // Restart counter
 
         btnNext.innerHTML = "Sprawdź odpowiedzi";
-        var questionBox = document.getElementById("id__questionBox");
+        let questionBox = document.getElementById("id__questionBox");
         showStats (answerParentDiv,questionBox, userScore, numQuery);
     }
 }
 
 /*  Timer Function (countdown/critical time/stop game)
-*   @param: time - number (eg. read from JSON file)
+*   @param: {number} - time (eg. read from JSON file)
 *                  !default - number of questions multipled by 30 sec.
 */
-function timer(time = numQuery*30 ){ 
-    var timer = document.getElementById("id__timer");
-    var tm = time;
-    var interval = setInterval(function() {
+const timer = function timer(time = numQuery*30 ){ 
+    let timer = document.getElementById("id__timer");
+    let tm = time;
+    let criticalTime = 30 // Signal the end of time (default 30 sec.)
+    let interval = setInterval(function() {
         tm--;
         timer.innerHTML = tm;
-        if ( tm <= 30 ){
+        if ( tm <= criticalTime ){
             timer.className = "sg-text-bit  sg-text-bit--not-responsive sg-text-bit--small  quiz__text--warning";
         }
         if (tm === 0 ){
@@ -111,65 +110,60 @@ function timer(time = numQuery*30 ){
 })();
 
 /*
-*   @param: element - DOM string
-*   @param: list - string array
-*   @param: check - boleans
+*   @param: {DOM string} - element
+*   @param: {string array}- list
+*   @param: {boleans} - check
 */
-function showAnswer (element, list, check = false) {
-    var msg = "";
-    for(var i = 0; i < list.length; i++){
+function showAnswer (element, list, check) {
+    let msg = "";
+    list.forEach( function(e,i){
         if(check) {
             if(list[i].correct){
-                msg += '<p class="quiz__answer quiz__answer--correct" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
+                msg += `<p class="quiz__answer quiz__answer--correct" data-answer="${list[i].id}" >${list[i].answer}</p>`;
+            } else {
+                msg += `<p class="quiz__answer" data-answer="${list[i].id}" >${list[i].answer}</p>`;
             }
-            else {
-                msg += '<p class="quiz__answer" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
-            }
+        } else{
+            msg += `<p class="quiz__answer" data-answer="${list[i].id}" >${list[i].answer}</p>`;
         }
-        else{
-            msg += '<p class="quiz__answer" data-answer="'+list[i].id+'" >'+list[i].answer+'</p>';
-        }
-    }
+    })
     element.innerHTML = msg;
 }
 
 /* 
-*   @param: id_answer - number
-*   @param: list - string array
+*   @param: {number} - id_answer 
+*   @param: {string array} - list
 */
 function checkAnswer (id_answer, list) {
-    if(list[id_answer].correct){
-        return 1;
-    }  
-    return 0;
+    return list[id_answer].correct
 }
 
 /*  Create question in view
-*   @param: element - DOM string
-*   @param: str - string
+*   @param: {DOM string} - element 
+*   @param: {string} - str
 */
 function showQuestion (element,  str) {
     element.innerHTML = str;    
 }
 
 /* Show statistic in final screen
-*   @param: element1 - DOM string (output)
-*   @param: element2 - DOM string
-*   @param: score - number
-*   @param: maxNum - number
+*   @param: {DOM string} - element1 (output)
+*   @param: {DOM string} - element2
+*   @param: {number} - score 
+*   @param: {number} - maxNum
 */
 function showStats (element1, element2, score, maxNum) {
-    element1.innerHTML = '<h1 class="sg-text-bit sg-text-bit--not-responsive sg-text-bit--warning quiz__text--center"> Twój wynik: '+score+"/"+maxNum+'</h1>';
+    element1.innerHTML = `<h1 class="sg-text-bit sg-text-bit--not-responsive sg-text-bit--warning quiz__text--center"> Twój wynik: ${score}/${maxNum}</h1>`;
     element2.style.display = 'none';
 }
 
 /*  Read file from URL and return data
-*   @param: file - url adrress
-*   @return: DOM string or false(error)
+*   @param:  {url adrress} - file
+*   @return: {DOM string} or {false}(error)
 */
 function readFile (urlJSON) {
-    var xhr = new XMLHttpRequest();
-    var data;
+    let xhr = new XMLHttpRequest();
+    let data;
     xhr.overrideMimeType("application/json");
     xhr.open("GET", urlJSON, false); // Synchronic read file, important data for run app  
     xhr.onreadystatechange = function() {
@@ -189,11 +183,11 @@ function readFile (urlJSON) {
 }
  
 /*  Function show progress in game (left-bottom div in footer)
-*   @param: element - DOM string
-*   @param: counter - number
-*   @param: maxNum - number
+*   @param:  {DOM string} - element
+*   @param:  {number} - counter
+*   @param:  {number} - maxNum 
 */
 function updateProgress(element, counter, maxNum){
-    var msg = counter+"/"+maxNum;
+    let msg = `${counter}/${maxNum}`;
     element.innerHTML = msg;
 }
